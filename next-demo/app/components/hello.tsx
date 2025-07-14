7a1a260d3884e6a4af8e9ba57d27b28f5cc66741
sarank1634 <saravanakumar1634@gmail.com>
2025-07-14 18:45:16 +0530

    get user id routers

---
+++ next-demo/app/components/hello.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+function Hello() {
+    console.log("I am a Client component");
+    return(
+        <>
+        <div>
+            <h1>Welcome to my website</h1>
+            
+        </div>
+        </>
+    )}
+    export default Hello;
---
+++ next-demo/app/dashboard/users/[id]/page.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+export default function Users( {params} : {params: {id: string}}) {
+    const {id} = params;
+    return (
+        <div>
+            <h1>users Details page</h1>
+            <h1>
+                User profile: {id};
+            </h1>
+        </div>
+    );
+}
+
+// use params to get users id which users clicked
+// declare params type
