e01d126c37a05499576e4cd4892c2bfbc95c2c45
sarank1634 <saravanakumar1634@gmail.com>
2025-07-15 18:17:09 +0530

    configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./yc_directory/app/**/*.{js,ts,jsx,tsx}",
+    "./yc_directory/pages/**/*.{js,ts,jsx,tsx}",
+    "./yc_directory/components/**/*.{js,ts,jsx,tsx}",
+    "./yc_directory/src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
