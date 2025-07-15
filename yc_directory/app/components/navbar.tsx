1ea1e3fb847cbcc966eec2d25d9771a17042079e
sarank1634 <saravanakumar1634@gmail.com>
2025-07-15 18:03:56 +0530

    github login finish

--- yc_directory/app/components/navbar.tsx
+++ yc_directory/app/components/navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
         <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
             <nav className="flex justify-between items-center">
                 <Link href="/">
-                    <img src="../public/logo.png" alt="" width={144} height={30} />
+                    <img src="/logo.png" alt="Logo" width={144} height={30} />
                 </Link>
                 <div className="flex items-center gap-5">
                     {session && session.user ? (
@@ -17,9 +17,10 @@ const Navbar = () => {
                             <Link href="/startup/create">
                                 <span>Create</span>
                             </Link>
+
                             <button onClick={() => signOut()}>
-                                <span>Logout</span>
-                            </button>
+    <span>Logout</span>
+</button>
                             <Link href={`/user/${session.user.id}`}>
                                 <span>{session.user.name}</span>
                             </Link>
