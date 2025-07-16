1d6624496630c8b5ee852e5be2e69113cd9c5607
sarank1634 <saravanakumar1634@gmail.com>
2025-07-16 10:49:47 +0530

    font resloved

--- yc_directory/app/(ROOT)/page.tsx
+++ yc_directory/app/(ROOT)/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
+import Navbar from "../components/navbar";
 
 export default function Home() {
   return (
     <>
-    <h1 className="text-3xl">Home</h1>
-    
-    
+      <Navbar />
+      <h1 className="uppercase bg-black px-6 y-3  fomt-work-sans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5">Home</h1>
     </>
-    );
+  );
 }
--- yc_directory/app/layout.tsx
+++ yc_directory/app/layout.tsx
@@ -1,47 +1,47 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 
-const workSans = localFont({
-  src: [
-    {
-      path: "./assets/fonts/WorkSans-Regular.ttf",
-      weight: "900",
-      style: "normal",
-    },{
-      path: "./assets/fonts/WorkSans-BLack.ttf",
-      weight: "900",
-      style: "normal",
-    },
-    {
-      path: "./assets/fonts/WorkSans-Bold.ttf",
-      weight: "900",
-      style: "normal",
-    },{
-      path: "./assets/fonts/WorkSans-SemiBold.ttf",
-      weight: "500",
-      style: "normal",
-    },{
-      path: "./assets/fonts/WorkSans-Medium.ttf",
-      weight: "900",
-      style: "normal",
-    },{
-      path: "./assets/fonts/WorkSans-ExtraLight.ttf",
-      weight: "900",
-      style: "normal",
-    },{
-      path: "./assets/fonts/WorkSans-Light.ttf",
-      weight: "900",
-      style: "normal",
-    },{
-      path: "./assets/fonts/WorkSans-Regular.ttf",
-      weight: "900",
-      style: "normal",
-    },
-  ],
-  variable: "--font-work-sans",
-});
+// export const workSans = localFont({
+//   src: [
+    // {
+    //   path: "./assets/fonts/WorkSans-Regular.ttf",
+    //   weight: "900",
+    //   style: "normal",
+    // },
+    // {
+    //   path: "./assets/fonts/WorkSans-BLack.ttf",
+    //   weight: "900",
+    //   style: "normal",
+    // },
+    // {
+    //   path: "./assets/fonts/WorkSans-Bold.ttf",
+    //   weight: "900",
+    //   style: "normal",
+    // },
+    // // Removed missing WorkSans-SemiBold.ttf font
+    // {
+    //   path: "./assets/fonts/WorkSans-Medium.ttf",
+    //   weight: "900",
+    //   style: "normal",
+    // },{
+    //   path: "./assets/fonts/WorkSans-ExtraLight.ttf",
+    //   weight: "900",
+    //   style: "normal",
+    // },{
+    //   path: "./assets/fonts/WorkSans-Light.ttf",
+    //   weight: "900",
+    //   style: "normal",
+    // },
+    // {
+    //   path: "./assets/fonts/WorkSans-Regular.ttf",
+    //   weight: "900",
+    //   style: "normal",
+    // },
+//   ],
+//   variable: "--font-work-sans",
+// });
 
 export const metadata: Metadata = {
   title: "YC",
